7c2ef8796c6149ebfba1fad536cdce6f1b0ba8d3
paabes <50769912+paabes@users.noreply.github.com>
2021-04-01 19:09:54 +0300

    fully working code + all test cases

---
+++ Coursework1/Coursework1.cpp
@@ -0,0 +1,14 @@
+// Coursework1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+
+#include "Main.h"
+#include "Item.h"
+
+
+int main()
+{
+	start();
+	return 0;
+}
